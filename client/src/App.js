1f0886b75a5bbd0ac287feb995358557e16b2acc
Andrew Locke <alocke12992@gmail.com>
2018-03-12 11:08:11 -0600

    Adds 404

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About'; 
-
+import NoMatch from './components/NoMatch';
 
 const App = () => (
   <div>
-    <Route exect path="/" component={Home} />
-    <Route exect path="/about" component={About} />
+    <switch>
+      # Your home route MUST have "exact" or it will render all pages with "/"
+      <Route exect path="/" component={Home} />
+      <Route exect path="/about" component={About} />
+      # a route with no path is ALWAYS RENDERED
+      #MUST BE THE LAST ROUTE IN THE LIST OF ROUTES
+      <Route component={NoMatch} /> 
+    </switch>
   </div>
 )
 
